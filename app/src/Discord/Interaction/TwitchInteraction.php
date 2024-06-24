bb096ce1fcc7730b88825045b9876fd40f93ee30
Baptiste Leduc <baptiste.leduc@gmail.com>
2024-06-24 18:18:10 +0200

    Add OAuth log

--- app/src/Discord/Interaction/TwitchInteraction.php
+++ app/src/Discord/Interaction/TwitchInteraction.php
@@ -14,6 +14,7 @@ use React\Promise\PromiseInterface;
 use React\Socket\SocketServer;
 use FastRoute\RouteCollector;
 use React\Http\Message\Response;
+use Symfony\Component\HttpClient\CurlHttpClient;
 use function FastRoute\simpleDispatcher;
 use function React\Promise\resolve;
 
@@ -33,6 +34,9 @@ final readonly class TwitchInteraction implements InteractionInterface
 
     public function __construct(
         private int $reactHttpServerPort,
+        private string $twitchAppClientId,
+        private string $twitchAppClientSecret,
+        private string $twitchAppCallback,
     ) {
     }
 
@@ -41,10 +45,11 @@ final readonly class TwitchInteraction implements InteractionInterface
         // @see https://agiroloki.medium.com/streaming-reactphp-in-reactjs-2cda05de3b73
         $http = new HttpServer(function (ServerRequestInterface $request) use ($discord): PromiseInterface {
             $dispatcher = simpleDispatcher(
-                function (RouteCollector $route) use ($request, $discord) {
-                    $route->get('/hook/eventsub/online', fn () => $this->eventOnline($request, $discord));
+                function (RouteCollector $collector) use ($request, $discord) {
+                    $collector->addRoute('GET', '/oauth/code', fn () => $this->codeOauth($request));
+                    $collector->addRoute('GET', '/hook/eventsub/online', fn () => $this->eventOnline());
 
-                    return $route;
+                    return $collector;
                 },
             );
             $response = $dispatcher->dispatch(
@@ -68,7 +73,7 @@ final readonly class TwitchInteraction implements InteractionInterface
                     new Response(
                         StatusCodeInterface::STATUS_NOT_FOUND,
                         array_merge(self::JSON_TYPE, self::DEFAULT_HEADERS),
-                        json_encode(['message' => 'Route not found']),
+                        json_encode(['message' => 'Route not found: ' . $request->getUri()->getPath()]),
                     ),
                 );
             }
@@ -80,13 +85,68 @@ final readonly class TwitchInteraction implements InteractionInterface
         $http->listen($socket);
     }
 
-    private function eventOnline(ServerRequestInterface $request, Discord $discord): PromiseInterface
+    private function codeOauth(ServerRequestInterface $request): PromiseInterface
     {
-        $contents = $request->getBody()->getContents();
-        $webhook = json_decode($contents, true);
+        $queryParams = $request->getQueryParams();
 
-        if ('stream.online' === $webhook['type']) {
-            $discord->getChannel('1229375406797357076')->sendMessage(MessageBuilder::new()->setContent('STREAMER IS LIVE'));
+        $client = new CurlHttpClient(['base_uri' => 'https://id.twitch.tv']);
+        $response = $client->request('POST', '/oauth2/token', [
+            'body' =>
+                sprintf(
+                    'client_id=%s&client_secret=%s&code=%s&grant_type=authorization_code&redirect_uri=%s',
+                    $this->twitchAppClientId,
+                    $this->twitchAppClientSecret,
+                    $queryParams['code'],
+                    $this->twitchAppCallback
+                )
+            ]
+        );
+
+        return resolve(
+            new Response(
+                StatusCodeInterface::STATUS_OK,
+                \array_merge(self::TEXT_TYPE, self::DEFAULT_HEADERS),
+                $response->getContent(false),
+            ),
+        );
+    }
+
+    private function eventOnline(): PromiseInterface
+    {
+        return resolve(
+            new Response(
+                StatusCodeInterface::STATUS_OK,
+                \array_merge(self::TEXT_TYPE, self::DEFAULT_HEADERS),
+                'OK'
+            ),
+        );
+        
+        $contents = $request->getBody()->getContents();
+        $webhookContents = json_decode($contents, true);
+
+//        $discord->getChannel('1229375406797357076')->sendMessage(MessageBuilder::new()->setContent(json_encode($request->getHeaders())));
+//        $discord->getChannel('1229375406797357076')->sendMessage(MessageBuilder::new()->setContent($contents));
+
+        if ('webhook_callback_verification' === $request->getHeaderLine('Twitch-Eventsub-Message-Type')) {
+//            $discord->getChannel('1229375406797357076')->sendMessage(MessageBuilder::new()->setContent(sprintf('VALIDATION - %s', $webhookContents['challenge'])));
+
+            return resolve(
+                new Response(
+                    StatusCodeInterface::STATUS_OK,
+                    \array_merge(self::TEXT_TYPE, self::DEFAULT_HEADERS),
+                    $webhookContents['challenge']
+                ),
+            );
+        } else {
+            return resolve(
+                new Response(
+                    StatusCodeInterface::STATUS_OK,
+                    \array_merge(self::TEXT_TYPE, self::DEFAULT_HEADERS),
+                    'OK'
+                ),
+            );
+
+//            $discord->getChannel('1229375406797357076')->sendMessage(MessageBuilder::new()->setContent('STREAMER IS LIVE'));
         }
 
         return resolve(
